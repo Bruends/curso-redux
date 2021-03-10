55d89fc90c01013be8f0751794c308dfe750db1e
Bruno Mendes <brunomendes962@gmail.com>
2021-03-10 10:44:16 -0300

    Aula 0302 Middlewares

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const li = Array.from(document.querySelectorAll('li'))
-
-const getElementAttr = (key) => {
-  return function (el) {
-    return el.getAttribute(key)
+function reducer(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENTAR':
+      return state + 1
+    case 'REDUZIR':
+      return state - 1
+    default:
+      return state
   }
 }
 
-const getAttrDataSlide = getElementAttr('data-slide')
-const getAttrId = getElementAttr('id')
+// middleware
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+  console.log('action', action)
+  console.log('Prev_State', store.getState())
+  const actionResult = next(action)
+  console.log('New_State', store.getState())
+  console.groupEnd()
+  return actionResult
+}
 
-const dataSlideList = li.map(getAttrDataSlide)
-const idList = li.map(getAttrId)
+const { applyMiddleware, compose } = Redux
 
-console.log(dataSlideList)
-console.log(idList)
+const composeEnhancers =
+  window.__REDUX__DEVTOOLS_EXTENSION__COMPOSE__ || compose
+
+const middleware = composeEnhancers(applyMiddleware(logger))
+
+const store = Redux.createStore(reducer, middleware)
+
+store.dispatch({ type: 'INCREMENTAR' })
+console.log(store.getState())
